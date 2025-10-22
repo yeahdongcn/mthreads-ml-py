ede28c294939981a10eed52ca5c85fd6d971703f
Xiaodong Ye <xiaodong.ye@mthreads.com>
2025-10-22 14:08:47 +0800

    1
    Signed-off-by: Xiaodong Ye <xiaodong.ye@mthreads.com>

---
+++ example.py
@@ -0,0 +1,19 @@
+#
+# Sample script to demonstrate the usage of NVML API python bindings
+#
+
+# To Run:
+# $ python ./example.py
+
+from pymtml import *
+
+#######
+def deviceQuery():
+
+    strResult = ''
+
+    return strResult
+
+# If this is not executed when module is imported
+if __name__ == "__main__":
+    print(deviceQuery())
